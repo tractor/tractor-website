58404d57941a18a3de992202a1dde0c2bd5ca4b1
Jon Clayden <code@clayden.org>
2017-04-14 00:55:05 +0100

    Generalising upload script to also handle docs target

--- upload.rb
+++ upload.rb
@@ -3,28 +3,40 @@
 require "tmpdir"
 
 if ARGV.length < 1
-    abort("Usage: ./upload.rb <files>")
+    abort("Usage: ./upload.rb [-d] <files>")
 end
 
 status = true
+to_docs = false
+
+if ARGV[0] == "-d"
+    to_docs = true
+    args = ARGV.drop(1)
+else
+    args = ARGV
+end
 
 Dir.mktmpdir do |tmpdir|
-    ARGV.each do |filename|
+    args.each do |filename|
         basename = File.basename(filename)
-        if filename =~ /\.html$/
-            html = File.open(filename) { |file| file.read }
-            html.gsub!(/\"([\w\-]+)\.html([\"\#])/, '"\1\2')
-            file = File.open(tmpdir + "/" + basename, "w") { |file| file.write(html) }
-        elsif filename.count("/") > 0
+        if filename.count("/") > 0
             subdir = File.join(tmpdir, File.dirname(filename))
             Dir.mkdir(subdir) unless Dir.exist? subdir
             FileUtils.copy(filename, subdir)
+        elsif filename =~ /\.html$/ and not to_docs
+            html = File.open(filename) { |file| file.read }
+            html.gsub!(/\"([\w\-]+)\.html([\"\#])/, '"\1\2')
+            file = File.open(tmpdir + "/" + basename, "w") { |file| file.write(html) }
         else
             FileUtils.copy(filename, tmpdir)
         end            
     end
     
-    status = system("scp -r #{tmpdir}/* `cat upload_target`")
+    if to_docs
+        status = system("cp -Rv #{tmpdir}/* `cat docs_target`")
+    else
+        status = system("scp -r #{tmpdir}/* `cat upload_target`")
+    end
 end
 
 exit(status)
